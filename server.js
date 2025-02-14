541906b9a4e5b7e55d1167708377f951946f47ae
Sudhanand Tiwari <sudhanand.1488@gmail.com>
2025-02-14 07:46:09 +0530

    latest update

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 //routes
 app.use('/api/v1/auth', authRoutes);
@@ -30,16 +31,7 @@ app.use('/api/v1/category',categoryRoutes)
 app.use('/api/v1/product',productRoutes)
 
 //rest api 
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to ecommerce app</h1>");
-});
-
-//static files
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname,'./client/build')));
-
-app.get("*", function(req,res){
+app.use("*", function(req,res){
     res.sendFile(path.join(__dirname,"./client/build/index.html"));
 });
 
